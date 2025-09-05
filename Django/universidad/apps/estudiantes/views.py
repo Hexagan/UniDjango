1b64c7beb3754b6489c3720f72205215a31dec93
Federico <38073413+Hexagan@users.noreply.github.com>
2025-09-05 20:49:08 -0300

    Delete Django directory

--- Django/universidad/apps/estudiantes/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Estudiante
-
-def lista_estudiantes(request):
-    estudiantes = Estudiante.objects.all()
-    return render(request, "estudiantes/lista_estudiantes.html", {"estudiantes": estudiantes})
--- Django/universidad/apps/materias/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Materia
-
-def lista_materias(request):
-    materias = Materia.objects.all()
-    return render(request, "materias/lista_materias.html", {"materias": materias})
